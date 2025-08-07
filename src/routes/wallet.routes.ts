ac9501342ad244687063b859b9616afa64002398
shashank <shashankraj007281@gmail.com>
2025-08-07 17:48:47 +0530

    can make boost view plan purchases

--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { topupCoins } from '../controllers/topup-coin.controller.js';
 import { WalletController } from '../controllers/wallet.controller.js';
+import { makeOrder } from 'controllers/order.controller.js';
 
 const router = express.Router();
 
 router.get('/:userId', WalletController.getWallet);
 router.patch('/:userId', WalletController.updateWalletBalance);
 router.post("/topup", topupCoins);
+router.post("/order", makeOrder)
 
 export default router;
