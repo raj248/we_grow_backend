781e75dc187f819f77a9cc12f9fdb895dece4c34
shashank <shashankraj007281@gmail.com>
2025-08-14 15:36:49 +0530

    bug fixes

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -70,7 +70,7 @@ export async function processEarning(token: string) {
     const { userId, orderId } = verifiedData;
 
     // Update user wallet
-    await WalletModel.rewardWithTransaction(userId, REWARD_AMOUNT, token.slice(0, 10));
+    await WalletModel.rewardWithTransaction(userId, REWARD_AMOUNT, token.slice(-10));
     setLastUpdated(cacheKeys.wallet(userId))
     setLastUpdated(cacheKeys.transactionInfo(userId))
     // Add to watch history
